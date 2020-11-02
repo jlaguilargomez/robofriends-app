96539fdb9f495e5e9e2344af9443be98a6fd629d
jlaguilargomez <jlaguigom@gmail.com>
2020-11-02 10:48:40 +0100

    Set communication between components. Implements search functionallity

--- src/App.js
+++ src/App.js
@@ -3,14 +3,32 @@ import CardList from './components/card-list/CardList';
 import SearchBox from './components/search-box/SearchBox';
 import { robots } from './core/robots/robots';
 
-const App = () => {
-  return (
-    < div className="tc" >
-      <h1>RoboFriends</h1>
-      <SearchBox></SearchBox>
-      <CardList robots = {robots}></CardList>
-    </div>
-  )
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: robots,
+      searchfields: '',
+    };
+  }
+
+  // Recuerda que usamos un ARROW FUNCTION para que obtenga el THIS del "sitio" en el que se ha creado, NO de donde se invoca (en este caso en SearchBox Component)
+  onSearchChange = (event) => {
+    this.setState({searchfields: event.target.value})
+    
+  }
+  render() {
+    const filteredRobots = this.state.robots.filter((robots) =>
+      robots.name.toLowerCase().includes(this.state.searchfields.toLowerCase())
+    );
+    return (
+      <div className="tc">
+        <h1>RoboFriends</h1>
+        <SearchBox searchChange={this.onSearchChange}></SearchBox>
+        <CardList robots={filteredRobots}></CardList>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/search-box/SearchBox.js
+++ src/components/search-box/SearchBox.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const SearchBox = () => {
+const SearchBox = ({searchField, searchChange}) => {
   return (
-    <div class="pa2">
-      <input class="pa3 ba b--greeen bg-lightest-blue" type="search" placeholder="search robots"/>
+    <div className="pa2">
+      <input className="pa3 ba b--greeen bg-lightest-blue" type="search" placeholder="search robots" onChange={searchChange}/>
     </div>
   );
 }
